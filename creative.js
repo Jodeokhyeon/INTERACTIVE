a7550e3caf04958a8ad3d1e27dff0af03975ef7c
JODEOKHYEON <102511077+Jodeokhyeon@users.noreply.github.com>
2025-04-11 22:47:53 +0900

    Update creative.js

--- creative.js
+++ creative.js
@@ -1,14 +1,14 @@
+// 핵심 변수들
 let foot = [];
 let footprints = [];
 let traceImages = [];
 let footIndex = 0;
 
 let scrollY = 0;
-let scrollTargetY = 0;
-let isScrollingY = false;
-
 let scrollX = 0;
+let scrollTargetY = 0;
 let scrollTargetX = 0;
+let isScrollingY = false;
 let isScrollingX = false;
 
 let currentDirection;
@@ -22,23 +22,35 @@ let hasSongEndedNaturally = false;
 let zoom = 1.0;
 let zoomTarget = 1.0;
 
+let isReplaying = false;
+let replayIndex = 0;
+let replayTimer = 0;
+let replayInterval = 300;
+
 function preload() {
-  foot[0] = loadImage('foot 1.png');
-  foot[1] = loadImage('foot 2.png');
-  stepSound = loadSound('Billy Joel - Piano Man.mp3');
+  foot[0] = loadImage("foot 1.png");
+  foot[1] = loadImage("foot 2.png");
+  stepSound = loadSound("Billy Joel - Piano Man.mp3");
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight); // ✅ 아이패드 대응
+  createCanvas(windowWidth, windowHeight);
   imageMode(CENTER);
-  noStroke();
   smooth();
+  noStroke();
 
   currentDirection = createVector(0, -1);
   createFirstFoot();
 
-  let btn = select('#resetButton');
-  btn.mousePressed(resetJourney);
+  select("#resetButton").mousePressed(resetJourney);
+  select("#endButton").mousePressed(() => {
+    isEnded = true;
+    hasSongEndedNaturally = true;
+    isReplaying = true;
+    replayIndex = 0;
+    replayTimer = millis();
+    footprints = [];
+  });
 }
 
 function windowResized() {
@@ -59,29 +71,23 @@ function draw() {
 
   if (isEnded && (footprints.length + traceImages.length) > 0) {
     let all = footprints.concat(traceImages);
-
     let minX = Infinity, maxX = -Infinity;
     let minY = Infinity, maxY = -Infinity;
-
     for (let t of all) {
       minX = min(minX, t.x);
       maxX = max(maxX, t.x);
       minY = min(minY, t.y);
       maxY = max(maxY, t.y);
     }
-
     let centerX = (minX + maxX) / 2;
     let centerY = (minY + maxY) / 2;
-
     let traceWidth = maxX - minX;
     let traceHeight = maxY - minY;
-
     let margin = 200;
     let scaleX = (width - margin) / traceWidth;
     let scaleY = (height - margin) / traceHeight;
     zoomTarget = min(scaleX, scaleY, 1.0);
-
-    translate((width / 2) / zoom - centerX, (height / 2) / zoom - centerY);
+    translate(width / 2 / zoom - centerX, height / 2 / zoom - centerY);
   } else {
     if (isScrollingY) {
       let diffY = scrollTargetY - scrollY;
@@ -91,7 +97,6 @@ function draw() {
         isScrollingY = false;
       }
     }
-
     if (isScrollingX) {
       let diffX = scrollTargetX - scrollX;
       scrollX += diffX * 0.05;
@@ -100,7 +105,6 @@ function draw() {
         isScrollingX = false;
       }
     }
-
     translate(-scrollX, -scrollY);
   }
 
@@ -109,16 +113,13 @@ function draw() {
   for (let i = footprints.length - 1; i >= 0; i--) {
     let fp = footprints[i];
     let now = millis();
-
     if (!fp.fadeOut) {
       let fadeInElapsed = now - fp.fadeInStart;
       fp.alpha = map(fadeInElapsed, 0, 1500, 0, 255, true);
     }
-
     if (fp.fadeOut) {
       let fadeOutElapsed = now - fp.fadeStart;
       fp.alpha = map(fadeOutElapsed, 0, 3000, 255, 0, true);
-
       if (fp.alpha <= 0) {
         traceImages.push({
           x: fp.x,
@@ -133,7 +134,6 @@ function draw() {
     }
 
     drawDreamyEffect(fp.x, fp.y, fp.alpha);
-
     push();
     translate(fp.x, fp.y);
     rotate(fp.angle);
@@ -142,15 +142,12 @@ function draw() {
     pop();
   }
 
-  for (let i = traceImages.length - 1; i >= 0; i--) {
-    let t = traceImages[i];
-    let alpha = 80;
-
+  for (let t of traceImages) {
     push();
     translate(t.x, t.y);
     rotate(t.angle);
     scale(0.95);
-    tint(255, alpha * 0.9);
+    tint(255, 80 * 0.9);
     image(t.img, 0, 0, 70, 90);
     pop();
   }
@@ -159,11 +156,34 @@ function draw() {
 
   if (isEnded) {
     drawEndingMessage();
+    if (isReplaying) {
+      replayFootprints();
+    }
+  }
+}
+
+function replayFootprints() {
+  if (replayIndex < traceImages.length) {
+    if (millis() - replayTimer > replayInterval) {
+      let t = traceImages[replayIndex];
+      footprints.push({
+        x: t.x,
+        y: t.y,
+        angle: t.angle,
+        img: t.img,
+        fadeOut: false,
+        fadeStart: null,
+        fadeInStart: millis(),
+        alpha: 0,
+        groupId: 999
+      });
+      replayTimer = millis();
+      replayIndex++;
+    }
   }
 }
 
 function touchStarted() {
-  // ✅ iOS 오디오 재생 활성화
   if (getAudioContext().state !== 'running') {
     getAudioContext().resume();
   }
@@ -173,64 +193,60 @@ function touchStarted() {
   let adjustedX = mouseX + scrollX;
   let adjustedY = mouseY + scrollY;
 
-  for (let i = 0; i < footprints.length; i++) {
-    let fp = footprints[i];
+  for (let fp of footprints) {
     let d = dist(adjustedX, adjustedY, fp.x, fp.y);
     if (d < 40 && !fp.fadeOut) {
       let groupIdToFade = fp.groupId;
 
-      for (let j = 0; j < footprints.length; j++) {
-        let other = footprints[j];
+      for (let other of footprints) {
         if (other.groupId === groupIdToFade && !other.fadeOut) {
           other.fadeOut = true;
           other.fadeStart = millis();
         }
       }
 
-      if (fp.y - scrollY < 150) {
-        scrollTargetY -= 300;
-        isScrollingY = true;
-      } else {
-        scrollTargetY -= 60;
-        isScrollingY = true;
-      }
+      if (fp.y - scrollY < 150) scrollTargetY -= 300;
+      else scrollTargetY -= 60;
+      isScrollingY = true;
 
-      if (fp.x - scrollX < 150) {
-        scrollTargetX -= 300;
-        isScrollingX = true;
-      }
+      if (fp.x - scrollX < 150) scrollTargetX -= 300;
+      if (fp.x - scrollX > width - 150) scrollTargetX += 300;
+      isScrollingX = true;
 
-      if (fp.x - scrollX > width - 150) {
-        scrollTargetX += 300;
-        isScrollingX = true;
-      }
+      if (!stepSound.isPlaying()) stepSound.play();
 
-      if (!stepSound.isPlaying()) {
-        stepSound.play();
-      }
-
-      if (!firstStepDone) {
-        setTimeout(() => {
-          if (!isEnded) {
+      setTimeout(() => {
+        if (!isEnded) {
+          if (!firstStepDone) {
             createFirstStepSet();
             firstStepDone = true;
-            stepSound.pause();
-          }
-        }, 2000);
-      } else {
-        setTimeout(() => {
-          if (!isEnded) {
+          } else {
             createNextFootSet();
-            stepSound.pause();
           }
-        }, 2000);
-      }
+          stepSound.pause();
+        }
+      }, 2000);
 
       break;
     }
   }
 
   return false;
+}
+
+function createFirstFoot() {
+  footprints.push({
+    x: width / 2,
+    y: height - 45,
+    angle: 0,
+    img: foot[footIndex],
+    fadeOut: false,
+    fadeStart: null,
+    fadeInStart: millis(),
+    alpha: 0,
+    groupId: groupCounter++
+  });
+  footIndex = (footIndex + 1) % foot.length;
 }
 
 function createFirstStepSet() {
@@ -242,11 +258,8 @@ function createNextFootSet() {
 }
 
 function createFootSet(stepSize, offsetX) {
-  let prev = footprints.length > 0
-    ? footprints[footprints.length - 1]
-    : { x: width / 2, y: height - 45, angle: 0 };
-
-  let thisGroupId = groupCounter++;
+  let prev = footprints[footprints.length - 1] || { x: width / 2, y: height - 45, angle: 0 };
+  let group = groupCounter++;
 
   for (let i = 0; i < 2; i++) {
     let isLeft = footIndex === 0;
@@ -257,18 +270,12 @@ function createFootSet(stepSize, offsetX) {
     while (!success && attempts < 15) {
       let angleChoice = random([-0.5, -0.25, 0, 0.25, 0.5]);
       currentDirection = createVector(sin(angleChoice), -cos(angleChoice)).normalize();
-
       let stepVec = p5.Vector.mult(currentDirection, stepSize);
       nextX = prev.x + (isLeft ? -offsetX : offsetX) + stepVec.x;
       nextY = prev.y + stepVec.y;
+      angle = prev.angle + random(-0.1, 0.1);
 
-      let prevAngle = prev.angle || 0;
-      angle = prevAngle + random(-0.1, 0.1);
-
-      let overlapping = footprints.some(fp =>
-        abs(nextX - fp.x) < 40 || abs(nextY - fp.y) < 40
-      );
-
+      let overlapping = footprints.some(fp => dist(fp.x, fp.y, nextX, nextY) < 40);
       if (!overlapping) success = true;
       attempts++;
     }
@@ -282,20 +289,17 @@ function createFootSet(stepSize, offsetX) {
       fadeStart: null,
       fadeInStart: millis(),
       alpha: 0,
-      groupId: thisGroupId
+      groupId: group
     });
-
     footIndex = (footIndex + 1) % foot.length;
     prev = { x: nextX, y: nextY, angle: angle };
-    stepCount++;
   }
 }
 
 function drawDreamyBackground() {
-  noStroke();
   let time = millis() * 0.0005;
-  let startY = scrollY - height * 1.5;
-  let endY = scrollY + height * 2;
+  let startY = scrollY - height * 5;
+  let endY = scrollY + height * 5;
 
   for (let y = startY; y < endY; y += 10) {
     let t = map(scrollY, 0, 3000, 0, 1);
@@ -305,8 +309,9 @@ function drawDreamyBackground() {
     let inter = map(y, startY, endY, 0, 1);
     let c1 = lerpColor(colorA, colorB, inter);
     let cFinal = lerpColor(c1, colorC, 0.5 + 0.5 * sin(y * 0.01 + time));
+    noStroke();
     fill(cFinal);
-    rect(-width * 2, y, width * 4, 10);
+    rect(-width * 5, y, width * 10, 10);
   }
 }
 
@@ -321,17 +326,10 @@ function drawEndingMessage() {
   push();
   resetMatrix();
   textAlign(CENTER, CENTER);
-  textSize(36);
+  textSize(45);
   fill(255, 240);
   text("여정이 끝났습니다", width / 2, height / 2);
   pop();
-}
-
-function keyPressed() {
-  if (key === 'T') {
-    isEnded = true;
-    hasSongEndedNaturally = true;
-  }
 }
 
 function resetJourney() {
@@ -351,27 +349,13 @@ function resetJourney() {
   zoomTarget = 1.0;
   isEnded = false;
   hasSongEndedNaturally = false;
+  isReplaying = false;
+  replayIndex = 0;
+  replayTimer = 0;
 
   if (stepSound.isPlaying()) {
     stepSound.stop();
   }
 
   createFirstFoot();
-}
-
-function createFirstFoot() {
-  let x = width / 2;
-  let y = height - 45;
-  footprints.push({
-    x: x,
-    y: y,
-    angle: 0,
-    img: foot[footIndex],
-    fadeOut: false,
-    fadeStart: null,
-    fadeInStart: millis(),
-    alpha: 0,
-    groupId: groupCounter++
-  });
-  footIndex = (footIndex + 1) % foot.length;
 }
